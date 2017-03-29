678f425c735e45b9f92eedd76655ae7e5d2fa9d3
su <101533613@qq.com>
2017-03-29 23:24:24 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,5 +5,5 @@ class Group < ApplicationRecord
   validates :title, presence: true
 
   has_many :group_relationships
-  has_many :members, through: :group_realtionships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
